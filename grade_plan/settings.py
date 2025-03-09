de4199e804a15cbf8fb22660b85ec6053c8e09d7
Jennie-ji <94039529+Jennie-ji@users.noreply.github.com>
2025-03-09 22:51:20 +0700

    Prepare to deploy
    will deploy on render
    add requirements.txt

--- grade_plan/settings.py
+++ grade_plan/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@q$8$hkvymc1cnefqrw1wt-qahizc80%+qkf3pocg=2=2y&3s$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
